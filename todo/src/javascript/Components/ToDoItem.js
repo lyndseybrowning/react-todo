2c58a42e3937ea3d414483eca0278115541ed506
Lyndsey Browning <lbrowning86@gmail.com>
2017-07-05 20:22:43 +0100

    Add comment regarding stateless component

--- todo/src/javascript/Components/ToDoItem.js
+++ todo/src/javascript/Components/ToDoItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+// notice how we're not using a class here
+// this is because, classes are only really useful when the
+// component needs to store state
+// since this is just a simple component, we can use a normal function 
+// that returns our JSX component.
+// props is passed automatically and is an object containing all props passed
+// to the component.
 const ToDoItem = (props) => {
     
     const isComplete = props.completed;
