33c547a11eb580d677cabca8eed5ec5cc283c015
Lyndsey Browning <lyndseybrowning@users.noreply.github.com>
2017-07-06 07:43:15 +0100

    Update comments

--- todo/src/javascript/Components/ToDoItem.js
+++ todo/src/javascript/Components/ToDoItem.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 // notice how we're not using a class here
-// this is because, classes are only really useful when the
-// component needs to store state
-// since this is just a simple component, we can use a normal function 
-// that returns our JSX component.
-// props is passed automatically and is an object containing all props passed
-// to the component.
+// classes are only really useful when the component needs to stores state
+// since this is just a simple component we can use a normal function 
+// that returns a JSX component.
+// props is an object containing all props passed to the component
 const ToDoItem = (props) => {
     
     const isComplete = props.completed;
